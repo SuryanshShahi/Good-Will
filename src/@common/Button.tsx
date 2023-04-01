96dddcbba437393c7a6468b3782d95ad06b5ad5c
SuryanshShahi <suryansh06shahi@gmail.com>
2023-04-01 19:44:40 +0530

    initial commit

---
+++ src/@common/Button.tsx
@@ -0,0 +1,13 @@
+export const Button = ({ name, bgColor, color, border }: any) => {
+  return (
+    <>
+      <button
+        className={`px-4 py-2 font-medium ${color || "text-white"} ${
+          border || null
+        } text-sm ${bgColor || "bg-green-400"}`}
+      >
+        {name}
+      </button>
+    </>
+  );
+};
